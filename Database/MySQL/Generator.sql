e75cb0124a542a4995a16ccbaeff5e87d629c6ee
premalnayee <premnayee1@gmail.com>
2020-05-03 01:16:07 +0100

    Issue #2 - Created database using MySQL. Just includes script, not database

---
+++ Database/MySQL/Generator.sql
@@ -0,0 +1,45 @@
+# drop them tables
+drop table if exists ProductOrderAdjudicator;
+drop table if exists Products;
+drop table if exists Orders;
+drop table IF EXISTS Customers;
+
+CREATE TABLE Customers (
+    CustID int NOT NULL,
+    FirstName varchar(100) NOT NULL,
+    LastName varchar(100) NOT NULL,
+    Address varchar(256),
+    EmailAddress varchar(100) NOT NULL,
+    PhoneNumber varchar(20) NOT NULL,
+    Age TINYINT NOT NULL,
+    PRIMARY KEY (CustID)
+); 
+
+CREATE TABLE Products (
+    ProdID int NOT NULL,
+    ProdName VARCHAR(128) NOT NULL,
+    Colour VARCHAR(32) NOT NULL,
+    BatterySize VARCHAR(16) NOT NULL,
+    Autopilot boolean NOT NULL,
+	DualMotor boolean NOT NULL,
+    ReleaseDate DATE,
+    ProductDelays VARCHAR(32) NOT NULL,
+    PRIMARY KEY (ProdID)
+); 
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    CustID int,
+    Warranty boolean NOT NULL,
+    PaymentMethod VARCHAR(32),
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (CustID) REFERENCES Customers(CustID)
+); 
+
+CREATE TABLE ProductOrderAdjudicator (
+	ProdID int NOT NULL,
+    OrderID int NOT NULL,
+    FOREIGN KEY (ProdID) REFERENCES game.products(ProdID),
+    FOREIGN KEY (OrderID) REFERENCES game.orders(OrderID),
+    UNIQUE(ProdID, OrderID)
+);
