14ce40240e12d28096d8c9d33477eaa35fbb1f41
premalnayee <premnayee1@gmail.com>
2020-05-03 15:09:45 +0100

    Issue #2 - Beautified the code using MySQL

--- Database/MySQL/Generator.sql
+++ Database/MySQL/Generator.sql
@@ -7,32 +7,29 @@ drop table if exists Products;
 drop table if exists Orders;
 drop table IF EXISTS Customers;
 
-# Create customers table
 CREATE TABLE Customers (
-    CustID int NOT NULL,
-    FirstName varchar(100) NOT NULL,
-    LastName varchar(100) NOT NULL,
-    Address varchar(256),
-    EmailAddress varchar(100) NOT NULL,
-    PhoneNumber varchar(20) NOT NULL,
+    CustID INT NOT NULL,
+    FirstName VARCHAR(100) NOT NULL,
+    LastName VARCHAR(100) NOT NULL,
+    Address VARCHAR(256),
+    EmailAddress VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
     Age TINYINT NOT NULL,
     PRIMARY KEY (CustID)
-); 
+);
 
-# Create Products table
 CREATE TABLE Products (
-    ProdID int NOT NULL,
+    ProdID INT NOT NULL,
     ProdName VARCHAR(128) NOT NULL,
     Colour VARCHAR(32) NOT NULL,
     BatterySize VARCHAR(16) NOT NULL,
-    Autopilot boolean NOT NULL,
-	DualMotor boolean NOT NULL,
+    Autopilot BOOLEAN NOT NULL,
+    DualMotor BOOLEAN NOT NULL,
     ReleaseDate DATE,
     ProductDelays VARCHAR(32) NOT NULL,
     PRIMARY KEY (ProdID)
-); 
+);
 
-# Create customers Orders
 CREATE TABLE Orders (
     OrderID INT NOT NULL,
     CustID INT,
@@ -40,14 +37,15 @@ CREATE TABLE Orders (
     PaymentMethod VARCHAR(32),
     PRIMARY KEY (OrderID),
     FOREIGN KEY (CustID)
-		REFERENCES Customers (CustID)
-); 
+        REFERENCES Customers (CustID)
+);
 
-# Create ProductOrdersLookup table to represent the many to many relationship between products and orders
 CREATE TABLE ProductOrderLookup (
-	ProdID int NOT NULL,
-    OrderID int NOT NULL,
-    FOREIGN KEY (ProdID) REFERENCES Products(ProdID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    UNIQUE(ProdID, OrderID)
+    ProdID INT NOT NULL,
+    OrderID INT NOT NULL,
+    FOREIGN KEY (ProdID)
+        REFERENCES Products (ProdID),
+    FOREIGN KEY (OrderID)
+        REFERENCES Orders (OrderID),
+    UNIQUE (ProdID , OrderID)
 );
