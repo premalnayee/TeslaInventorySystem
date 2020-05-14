34d9af9a3a4c4ef34a7038f180795886010136e6
premalnayee <premnayee1@gmail.com>
2020-05-14 23:48:54 +0100

    Added DBLink using JDBC and added some classes to represent the objects we will be storing

---
+++ Program/src/com/DBlink/DBConfig.java
@@ -0,0 +1,12 @@
+package com.DBlink;
+
+public final class DBConfig {
+
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/testspringer";
+	public static final String USER = "root";
+	public static final String PASS = "root";
+
+	private DBConfig() {
+	}
+
+}
